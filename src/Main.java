bed408b50b02fd7d70e12427e3a5a7a7cda8d7ce
RidiculousBuffal <3322946829@qq.com>
2023-08-03 17:26:58 +0800

    增加queryAlbumsForArtist

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
             System.out.println("Can't open dataSource!");
             return;
         }
-        List<Artist> artists = datasource.queryArtists();
+        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_ASC);
         if(artists==null)
         {
             System.out.println("No artists");
@@ -24,6 +24,16 @@ public class Main {
         {
             System.out.println("ID= "+artist.getId()+", Name= "+artist.getName());
         }
+
+        try{
+            List<String> albumsForArtist = datasource.queryAlbumsForArtist("Iron Maiden",Datasource.ORDER_BY_DESC);
+            for (String album : albumsForArtist) {
+                System.out.println(album);
+            }
+        }catch(NullPointerException e){
+            System.out.println("opps"+e.getMessage());
+        }
+
         datasource.close();
     }
 
